5235cdae797e51e1eae6db5a17313e552f82a7da
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-15 11:46:36 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.json("hello world")
+app.use('/user', (req, res) => {
+    res.send("HAHAHAHAHAHH")
 })
-app.use('/test', (req, res) => {
-    res.json("hello this test")
-})
-app.use('/hello', (req, res) => {
-    res.json('hello hello hello hel')
+app.get('/user', (req, res) => {
+    res.send({firstname:"sreeraj",lastname:"S R"})
 })
 
+app.post('/user', (req, res) => {
+    res.send("saved user details")
+})
+app.delete("/user", (req, res) => {
+    res.send("deleting user data from db")
+})
+app.use('/', (req, res) => {
+    res.send("hello from root")
+})
 app.listen(7777, () => {
     console.log('server is running on port 7777');
 })
