c4414de48a847f1ce0d5922eb087806f4c853b8e
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-15 13:06:08 +0530

    playing with multiple route handlers and next

--- src/app.js
+++ src/app.js
@@ -3,23 +3,38 @@ const express = require('express');
 const app = express();
 
 
-app.get('/user/:userId/:name/:password', (req, res) => {
+app.get('/user',[ (req, res,next) => {
+    console.log('first request handler')
+    // res.send('first response')
+    next()
+    
+}], [(req, res,next) => {
+    console.log('second request handler')
+    // res.send('second response')
+    next()
+}, (req, res,next) => {
+    console.log('third request handler')
+    res.send('third response')
 
-    res.send({firstname:"sreeraj",lastname:"S R",userId:req.params.userId,name:req.params.name,password:req.params.password})
-})
+}])
+ 
+
+// app.get('/user/:userId/:name/:password', (req, res) => {
+
+//     res.send({firstname:"sreeraj",lastname:"S R",userId:req.params.userId,name:req.params.name,password:req.params.password})
+// })
 
 // query params
 // app.get('/user', (req, res) => {
 //     res.send({firstname:"sreeraj",lastname:"S R",password:req.query.pass,userId:req.query.userId})
 // })
-
-app.post('/user', (req, res) => {
-    res.send("saved user details")
-})
-app.delete("/user", (req, res) => {
-    res.send("deleting user data from db")
-})
-app.use('/', (req, res) => {
+// app.post('/user', (req, res) => {
+//     res.send("saved user details")
+// })
+// app.delete("/user", (req, res) => {
+//     res.send("deleting user data from db")
+// })
+app.use('/user', (req, res) => {
     res.send("hello from root")
 })
 app.listen(7777, () => {
