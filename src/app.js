c5fab998686787f9b66d205c951cc9438a53973e
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-15 15:14:05 +0530

    wild card error handling and try catch exception handling

--- src/app.js
+++ src/app.js
@@ -4,23 +4,51 @@ const morgan = require('morgan')
 const app = express();
 
 app.use(morgan('dev'))
-app.use('/admin',adminAuth)
 
-app.get('/admin', (req, res) => {
-    res.send('admin dashboard')
+app.use('/admin', (req, res) => {
+    try {
+        if (1 === 1) {
+            throw new Error('1 is equal')
+        }
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send(error.message)
+    }
 })
 
-app.use('/user/home',userAuth,   (req, res) => {
-    res.send('User home page')
-})
+app.use("/user", (req, res) => {
 
-app.all('/user', (req, res) => {
-    res.send('login page')
+    if (1 === 1) throw new Error('not token')
+    else res.send('hai from home')
 })
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("something went wrong")
+    }
+})
+// app.use('/admin',adminAuth)
 
-app.get('/admin/getData', (req, res) => {
-    res.send("fetching all data from admin ")
-})
+// app.get('/admin', (req, res) => {
+//     res.send('admin dashboard')
+// })
+
+// app.use('/user/home',userAuth,   (req, res) => {
+//     res.send('User home page')
+// })
+
+// app.all('/user', (req, res) => {
+//     res.send('login page')
+// })
+
+// app.get('/admin/getData', (req, res) => {
+//     res.send("fetching all data from admin ")
+// })
+
+// app.use('/', (err, req, res, next) => {
+//     if (err) {
+//         res.status(500).send("something went wrong")
+//     }
+// })
 
 app.listen(7777, () => {
     console.log('server is running on port 7777');
