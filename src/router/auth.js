f2a9ad8e3b78a86c6925869bfaac18763b4d7ed3
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-17 11:47:15 +0530

    added logout api

--- src/router/auth.js
+++ src/router/auth.js
@@ -52,4 +52,12 @@ authRouter.post('/login', async (req, res) => {
     }
 
 })
+
+
+// logout api
+
+authRouter.post('/logout', (req, res) => {
+    res.cookie('token', null, { expires: new Date(Date.now()) })
+    res.send("Logut successfully")
+})
 module.exports = authRouter;
